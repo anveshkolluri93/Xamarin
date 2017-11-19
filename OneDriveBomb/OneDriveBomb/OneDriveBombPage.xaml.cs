8f791dd1a96d9d971ceed25d92e1c49eee9ec113
Anvesh Kolluri <Anveshkumar@Anveshs-MacBook-Pro.local>
2017-11-19 14:24:50 -0600

    first commit

---
+++ OneDriveBomb/OneDriveBomb/OneDriveBombPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace OneDriveBomb
+{
+    //Is like the main class
+    public partial class OneDriveBombPage : ContentPage
+    {
+        //Assigning bomb to a random button in the four buttons
+        static string bomb = new Random().Next(1, 5).ToString();
+        static int scores = 0;
+
+        public OneDriveBombPage() {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender, EventArgs e){
+            Button button = sender as Button;
+
+            if(button.Text == bomb){
+                await DisplayAlert("!!BOOM!! You are burn't.", "Game Over", "Retry");
+                bomb = new Random().Next(1, 5).ToString();
+                scores = 0;
+            }
+            else{
+                scores += 1;
+                await DisplayAlert("Bomb Diffused", "Score = " + scores, "Continue");
+                bomb = new Random().Next(1, 5).ToString();
+            }
+        }
+    }
+}
